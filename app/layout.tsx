68648d8cba2cdf3ab5dfc764a9e44e585f0a8c33
DYKY-S <yousrasab05@gmail.com>
2026-07-23 06:30:13 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,21 +31,31 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={`${bricolage.variable} ${inter.variable}`}>
-        {children}
-       <Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-YGDNVJT3VD"
-  strategy="afterInteractive"
-/>
 
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+      <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-KQDT3DM9"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
 
-    gtag('config', 'G-YGDNVJT3VD');
-  `}
-</Script>
+  {children}
+
+  <Script id="gtm" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];
+      w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;
+      j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-KQDT3DM9');
+    `}
+  </Script>
       </body>
     </html>
   );
